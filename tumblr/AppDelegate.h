c7befc46b972e3ccb18fa8554ede42272a5c9e54
Gabriel Valdivia <valdivia.gabriel@gmail.com>
2014-07-06 13:58:16 -0700

    Initial Commit

---
+++ tumblr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tumblr
+//
+//  Created by Gabriel Valdivia on 7/6/14.
+//  Copyright (c) 2014 Gabriel Valdivia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
